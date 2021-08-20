216c6d08bc99605f924ef55895ffa07f5446ce6a
wisdom <hahahihiho16@gmail.com>
2021-08-20 18:08:58 +0900

    update contracts add function getAll

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,13 +7,25 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
 
-contract NFT is ERC721URIStorage {
+contract NFT is ERC721URIStorage{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     address contractAddress;
 
     constructor(address marketplaceAddress) ERC721("Travel", "T") {
         contractAddress = marketplaceAddress;
+    }
+
+
+    function getAll() public view returns (address[] memory){
+        uint total_count = _tokenIds.current();
+        address[] memory ret = new address[](total_count+1);
+        for (uint i = 0; i <= total_count; i++) {
+            if(_exists(i)){
+                ret[i] = ownerOf(i);
+            }
+        }
+        return ret;
     }
 
     function createToken(string memory tokenURI) public returns (uint) {
